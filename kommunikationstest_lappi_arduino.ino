9ac8a94e889058c88a8939400dd965eb382b0e0a
bnjmnsbl <benjamin.seibel@gmx.de>
2018-05-09 13:40:27 +0200

    updates

--- kommunikationstest_lappi_arduino.ino
+++ kommunikationstest_lappi_arduino.ino
@@ -40,3 +40,17 @@ delay(2000); //wartet zwei Sekunden, damit man den Output lesen kann
 
 // ENDE LOOP-SCHLEIFE
 
+// ************* Fehlermeldung beim Hochladen etwa in deisem Stil: *************
+//
+//  avrdude: stk500v2_ReceiveMessage(): timeout
+//  avrdude: stk500v2_getsync(): timeout communicating with programmer
+//  Beim Hochladen des Sketches ist ein Fehler aufgetreten
+
+//**** Dann ist vermutlich ein Dragino-Shield mit GPS in Gebrauch. 
+//**** GPS nutzt die selben seriellen Pins wie die Arduino-IDE. 
+//**** Lösung: während des Hochladens das GPS-Reset-Knöpfchen 
+//**** gedrückt halten, dann geht es.
+//**** Quelle und Bild: 
+//**** http://wiki.dragino.com/index.php?title=Lora/GPS_Shield#Pin_Definition
+
+
